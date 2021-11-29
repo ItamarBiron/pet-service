74e6569af1fd39380606d01f64836856a9e632cd
itamar_biron <you@example.com>
2021-11-30 01:39:59 +0200

    add new controller - petController

---
+++ PetService.Api/Controllers/PetController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetService.Api.Controllers
+{
+    public class PetController : ControllerBase
+    {
+
+        public PetController()
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> getPets()
+        {
+            return Ok();
+        }
+        
+        
+        
+    }
+}
